cb0fbff502b1c4db09168c20947aff290e1c3576
AquaNeel <100383762+AquaNeel@users.noreply.github.com>
2023-03-30 16:54:18 -0400

    Add files via upload

---
+++ C-114/test1.py
@@ -0,0 +1,11 @@
+a = 33
+b = "33"
+if b > a :
+    print("B is greater than A")
+elif a == b :
+    print("A and B are both equal")
+    def my_function(fname):
+        print(fname + "Refnses")
+    my_function("Email")
+    my_function("Tobias")
+    my_function("Linus")
